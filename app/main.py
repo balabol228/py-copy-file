bd6a6964e313f00ebc1215258fb5f904e812aab3
Denys Baliuk <denuk14228@gmail.com>
2026-03-18 23:01:24 +0200

    first try copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) < 3:
+        return
+
+    file_name = parts[1]
+    new_file_name = parts[2]
+
+    if file_name == new_file_name:
+        return
+
+    with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
