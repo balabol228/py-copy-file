0a75cb9cd41d048f76c77c92a673f898dfbc31d4
Denys Baliuk <denuk14228@gmail.com>
2026-03-18 23:21:27 +0200

    third try copy file

--- app/main.py
+++ app/main.py
@@ -4,17 +4,15 @@ import os
 def copy_file(command: str) -> None:
     parts = command.split()
 
-    if len(parts) != 3 or parts[0] != "cp":
+    if len(parts) < 3 or parts[0] != "cp":
         return
 
     file_name = parts[1]
     new_file_name = parts[2]
 
-    if file_name == new_file_name or not os.path.isfile(file_name):
+    if file_name == new_file_name or not os.path.exists(file_name):
         return
 
-    try:
-        with open(file_name, "rb") as file_in, open(new_file_name, "wb") as f_out:
-            f_out.write(file_in.read())
-    except (PermissionError, OSError):
-        return
+    with open(file_name, "r") as file_in, open(new_file_name, "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
